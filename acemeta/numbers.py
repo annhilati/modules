17376a576cedfb2e741f67f7b215141221fa7a1d
Annhilati <antbe.artis@web.de>
2024-05-22 19:46:12 +0200

    Update numbers.py

--- acemeta/numbers.py
+++ acemeta/numbers.py
@@ -1,8 +1,8 @@
-def isPrime(num: int) -> bool:
-    if num < 2:
+def isPrime(n: int) -> bool:
+    if n < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
