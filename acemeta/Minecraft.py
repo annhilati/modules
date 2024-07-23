a77b35c162acef79122f9f29fb6685bc03257df3
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-23 09:43:13 +0200

    Sync

--- acemeta/Minecraft.py
+++ acemeta/Minecraft.py
@@ -1,3 +1,23 @@
+import requests as WEB
+
 class Player():
-    def __init__(self, name):
+    """
+    Contains information about a Minecraft player
+
+    #### Arguments:
+        name (str): The players Minecraft name
+
+    #### Raises:
+        ValueError: If no user with the given name exists
+    """
+    def __init__(self, name: str):
         self.name = name
+        self._url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
+        
+        response = WEB.get(self._url)
+
+        if response.status_code == 200:
+            data = response.json()
+            self.uuid = data['id']
+        elif response.status_code == 404:
+            raise ValueError(f"User with name '{name}' does not exist")       
